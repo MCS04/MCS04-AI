3e45fc565ccbfa19416db923a87d7babab895d6e
BrianNge341 <brianjinghong@gmail.com>
2023-07-06 14:56:42 +0800

    Added Demo for YOLO

---
+++ EBN-Grade-Classification/src/models/yolo v5/demo.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("yolov8x.pt")
+results = model.predict(source="0", show=True)
+print (results)
