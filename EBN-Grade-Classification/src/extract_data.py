4e2347b8054c7f1010d839872b197e3e31563b8e
Wong Nicholas <wongyiznicholas@gmail.com>
2023-07-06 14:43:40 +0800

    auto download from gdrive

--- EBN-Grade-Classification/src/extract_data.py
+++ EBN-Grade-Classification/src/extract_data.py
@@ -20,17 +20,14 @@ SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
           "https://www.googleapis.com/auth/drive.readonly"]
 
 
-def find_file_ids(d, creds):
+def find_file_ids(d, creds, folder_name):
     items = None
-    dataset_folder_id = "1QkXA-nHZDEvHyGyAlP1ufnAhba-hO6FI"
-    drive_id = "0AP5OUkOYBj0_Uk9PVA"
-
     try:
         service = build('drive', 'v3', credentials=creds)
 
         for key in d.keys():
             q_param = f"'{d[key]}' in parents and trashed=false"
-            results = service.files().list(pageSize=10, q=q_param, supportsAllDrives=True,
+            results = service.files().list(q=q_param, supportsAllDrives=True,
                                            includeItemsFromAllDrives=True, fields="nextPageToken, files(id, name)").execute()
             items = results.get('files', [])
             if not items:
@@ -39,7 +36,7 @@ def find_file_ids(d, creds):
             for idx, item in enumerate(items):
                 print(u'{0} ({1})'.format(item['name'], item['id']))
                 download_file(creds, item['id'], key,
-                              idx, idx == 0, item['name'])
+                               idx == 0, item['name'], folder_name)
 
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
@@ -48,22 +45,21 @@ def find_file_ids(d, creds):
     return items
 
 
-def download_file(creds, real_file_id, key, index, directory_flag, name):
+def download_file(creds, real_file_id, key, directory_flag, name, folder_name):
     try:
         # create drive api client
         service = build('drive', 'v3', credentials=creds)
 
         file_id = real_file_id
 
-        # IMPORTANT: YOUR DIRECTORY HAS TO BE THE SAME AS HOW THE GITHUB SHOULD BE, IE: D:\Year_3_Sem_1\FIT3161\Deep Learning\ebn-classification> with the ebn-classification at the end
         current_dir = os.getcwd()
         directory = os.path.join(
-            current_dir, f"{current_dir}\EBN-Grade-Classification\data\{key}")
+            current_dir, f"{current_dir}\EBN-Grade-Classification\{folder_name}\{key}")
 
         if directory_flag and not os.path.exists(directory):
             os.makedirs(directory)
 
-        file_path = f"{current_dir}/EBN-Grade-Classification/data/{key}/{name}"
+        file_path = f"{current_dir}/EBN-Grade-Classification/{folder_name}/{key}/{name}"
         if os.path.exists(file_path):
             return
 
@@ -82,7 +78,7 @@ def download_file(creds, real_file_id, key, index, directory_flag, name):
     if file is not None:
         result = file.getvalue()
 
-        with open(f"{current_dir}/EBN-Grade-Classification/data/{key}/{name}", "wb") as f:
+        with open(f"{current_dir}/EBN-Grade-Classification/{folder_name}/{key}/{name}", "wb") as f:
             f.write(result)
             print("Saved")
     else:
@@ -109,11 +105,19 @@ def get_credentials():
 
 def start_extracting():
     creds = get_credentials()
-    d = {"a": "1vRzFoK6DQGx3_88slAR5FgIZ3JFl29HB", "b": "1xeBvqFRJYFv88qoPp5KgK3t9EoTrJqaW",
-         "c": "1Hxytx8_7E2oc6KZ-uebkwqguR5gVKsZz", "d": "1FRULxbpH4tDA_LQDmocbvnLk6-XqrZB5", "others": "1iZnH8CBrtfnPMJHYneQ24iFEQkoYJRI1"}
+    grades = {"a": "1vRzFoK6DQGx3_88slAR5FgIZ3JFl29HB", "b": "1xeBvqFRJYFv88qoPp5KgK3t9EoTrJqaW",
+              "c": "1Hxytx8_7E2oc6KZ-uebkwqguR5gVKsZz", "d": "1FRULxbpH4tDA_LQDmocbvnLk6-XqrZB5", "others": "1iZnH8CBrtfnPMJHYneQ24iFEQkoYJRI1"}
+
+    colour = {"yellow": "1uy5cBbn5jwJoNOv9z7rTjsI-Bo1soD9C", "white": "1S-PqlSZoSKX1AtyKUUZgu6j-EZHMBC7I", "swamp-green": "1S-PqlSZoSKX1AtyKUUZgu6j-EZHMBC7I",
+              "pink": "1qC5wXjwrpIaNhOVZaUB4WO_tzkSbk_Fr", "off-white": "1_8-pLjiYTsofZXdrf-sDSYW4eMosy4RD", "mixture": "1Rl7vfDtGuUfCbyW_G6da9Iw4c9cY7aO-", "grey": "1waFcHdbr-GhBgNcUCAvHAQuVZHn0YNIs", "dark-green": "1XCS05lSOP0KNDaQ9RVqz_sjQI9J0JT8k", "beige": "1lyPDVm_0GSQI3QDnzjePwi1xYFxqoL5q"}
+
+    feather = {"abudant": "1FOKltn7kJ20HDQ65AgepKViukLfbkabE", "substantial": "1fLWjoyuYpsx96OPBWGr4wI7HS7-BPHLS",
+               "moderate": "1qWiARQ2AdNa3nbeCfZnGagbl7wqZ5VOa", "few": "1hdVxEgOfVY-SLfF_OQ0Ps6gjMWDmQFps", "minimal": "1V-k0JiRGVl89De_M6HR2o_VWZjHjuGOl"}
 
     # search for all the files in the respective folders
-    find_file_ids(d, creds)
+    find_file_ids(grades, creds, "grades")
+    find_file_ids(colour, creds, "colour")
+    find_file_ids(feather, creds, "feather")
 
 
 if __name__ == '__main__':
