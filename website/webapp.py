f8be0e2ff216a6dc54bc9fbade98e6e5b44556a4
Caleb Tan <calebtaneh@gmail.com>
2023-07-13 01:21:01 +0800

    Changed webapp layout to tab
    Incomplete. If need to change back for testing, uncomment the previous method or I can see what I can do tomorrow

--- website/webapp.py
+++ website/webapp.py
@@ -1,7 +1,8 @@
 from enum import Enum
 import gradio as gr
 import random as rand
-import keras
+import tensorflow as tf
+from tensorflow import keras
 import asyncio
 import numpy as np
 
@@ -22,6 +23,9 @@ def resnet50(img):
     class_label = labels[max_index]
     return class_label
 
+def yolov8(vid):
+    return vid
+
 
 def classify(img, model):
     result = ""
@@ -33,12 +37,32 @@ def classify(img, model):
 
     return f"This is Grade {result}" 
 
+
+with gr.Blocks() as demo:
+    gr.Markdown("Choose your model")
+    with gr.Tab("ResNet50"):
+        with gr.Row():
+            resnet_input = gr.Image(shape=(256, 256))
+            resnet_output = gr.Label(num_top_classes=3)    # missing path file /resnet50model
+        resnet_submit_button = gr.Button("Submit")
+    with gr.Tab("Yolov8"):
+        with gr.Row():
+            yolov8_input = gr.Video(source="webcam")    # add streaming=True or live=True? 
+            yolov8_output = gr.Label()
+        yolov8_button = gr.Button("Submit")
+
+    # resnet_submit_button.click(resnet50, inputs=resnet_input, outputs=resnet_output)
+    # yolov8_button.click(yolov8, inputs=yolov8_input, outputs=yolov8_output)
+
+if __name__ == "__main__":
+    demo.launch()
+
+
+# if want to test just uncomment and use this
+"""
 radio = gr.Radio([Model.RESNET50, Model.YOLOV8], label="Models")
 inputs = [gr.Image(
     shape=(256, 256)), radio]
 demo = gr.Interface(fn=classify, inputs=inputs,
                     outputs=gr.Label(num_top_classes=3))
-
-
-if __name__ == "__main__":
-    demo.launch()
+"""
