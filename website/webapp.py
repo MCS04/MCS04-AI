a43bf2b7ed1f1e1297e8e1053f0628b7eeb71356
Caleb Tan <calebtaneh@gmail.com>
2023-09-03 15:35:42 +0800

    Web application updates

--- website/webapp.py
+++ website/webapp.py
@@ -1,14 +1,21 @@
+# import sys
+
+# sys.path.insert(1, 'C:\Users\SC Wintech\Documents\mcs04\MCS04-AI\EBN-Grade-Classification')
+
 # Standard imports
+import gradio as gr
 from enum import Enum
 import numpy as np
 import random as rand
 import asyncio
 
+
 # Import for deep learning purposes
 import tensorflow as tf
 from tensorflow import keras
+from roboflow import Roboflow
 
-#test
+# test
 import requests
 
 # Download human-readable labels for ImageNet.
@@ -16,7 +23,6 @@ response = requests.get("https://git.io/JJkYN")
 labels = response.text.split("\n")
 
 # Gradio library import
-import gradio as gr
 
 
 class Model(Enum):
@@ -24,7 +30,10 @@ class Model(Enum):
     YOLOV8 = "yolov8"
 
 # Put ML Model here
+
+
 def resnet50(img):
+
     img = np.expand_dims(img, axis=0)
     model = keras.models.load_model(
         "../EBN-Grade-Classification/src/models/resnet50/resnet50model")
@@ -35,60 +44,83 @@ def resnet50(img):
 
     return f"This is Grade {class_label}"
 
-def yolov8(vid):
-    return vid
+
+def yolov8(img_path):
+    print("Running yolov8!")
+    rf = Roboflow(api_key="SuklAeRTbtD1k4yDlJsC")
+    project = rf.workspace().project("bird-nest-exr6l")
+    model = project.version(2).model
+
+    # infer on a local image
+    # print(model.predict("Test.jpg", confidence=40, overlap=30).json())
+
+    # visualize your prediction
+    # model.predict("Test.jpg", confidence=40, overlap=30).save("prediction.jpg")
+
+    predictions = model.predict(img_path, confidence=40, overlap=30)
+    predictions_json = predictions.json
+
+    print(predictions_json)
+
+    for bounding_box in predictions:
+        # x0 = bounding_box['x'] - bounding_box['width'] / 2#start_column
+        # x1 = bounding_box['x'] + bounding_box['width'] / 2#end_column
+        # y0 = bounding_box['y'] - bounding_box['height'] / 2#start row
+        # y1 = bounding_box['y'] + bounding_box['height'] / 2#end_row
+        class_name = bounding_box['class']
+        confidence_score = bounding_box['confidence']
+
+        detection_results = bounding_box
+        class_and_confidence = (class_name, confidence_score)
+        print(class_and_confidence, '\n')
+
+    predictions.save("prediction.jpg")
+
+    return class_and_confidence
+
+    # infer on an image hosted elsewhere
+    # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
 
 
 # test mock model
 inception_net = tf.keras.applications.MobileNetV2()
 
+
 def test_model(inp):
+    print("Running mobile net!")
     inp = inp.reshape((-1, 224, 224, 3))
     inp = tf.keras.applications.mobilenet_v2.preprocess_input(inp)
     prediction = inception_net.predict(inp).flatten()
     confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
-    return confidences
-
+    return [confidences, confidences]
 
 
 def classify(img, model):
+    print("Classified model!")
     result = ""
     if model == Model.RESNET50.value:
+        print("mobilenet")
         result = test_model(img)
 
     elif model == Model.YOLOV8:
-        result = ""
+        print("yolov8")
+        result = yolov8(img)
 
-    return result 
+    return result
 
-def test(a, b):
-    return "Model is " + b + "and output is " + a
 
-# image_input = gr.Image(shape=(256, 256))
-# video_input = gr.Video(source="webcam")
-# model_input_1 = gr.Dropdown(models, value=models[0], label="Model")
-# model_input_2 = gr.Dropdown(models, value=models[0], label="Model")
+def test(a):
+    try:
+        # Validate input (e.g., check if input_image is not None and of the correct type)
+        if a is None:
+            return "Invalid input: Image is missing or empty."
+        elif not isinstance(a, str):
+            return "Not a string"
 
-# image_outputs = gr.Label(num_top_classes=3)
-# video_outputs = gr.Label(num_top_classes=3)
-# title = "Edible Bird's Nest (EBN) Grade Classification System"
-# description = "<div align='center'>Created with object detection using deep learning models: YOLOv8 and ResNet50</div>\
-#                <div align='center'>Author: Caleb Tan, Christine Chiong, Wong Yi Zhen Nicholas & Brian Nge.</div>"
-# article = "<div align='center>Special thanks to Dr Lim Mei Kuan, Dr Chong Chun Yong & Dr Lai Weng Kin</div>"
-
-# upload_image_demo = gr.Interface(
-#     fn=test,
-#     inputs=[image_input, model_input_1],
-#     outputs=image_outputs,
-# )
-
-# video_stream_demo = gr.Interface(
-#     fn=test2,
-#     inputs=[video_input, model_input_2],
-#     outputs=video_outputs
-# )
-
-# demo = gr.TabbedInterface([upload_image_demo, video_stream_demo], ["Upload Image", "Real-time Video Streaming"], title=title)
+        # Replace this with your model's inference code
+        return "Success: " + a
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 
 models = ['resnet50', 'yolov8']
@@ -99,30 +131,35 @@ with gr.Blocks() as demo:
         <center><h1>Edible Bird's Nest (EBN) Grade Classification System</h1></center>
         <center><p>Created by Caleb Tan, Christine Chiong, Wong Yi Zhen Nicholas & Brian Nge</p></center>
         """
-        )
+    )
 
     with gr.Tab("Upload Image"):
         with gr.Row():
             with gr.Column():
-                image_input = gr.Image(shape=(224, 224))
-                image_model_input = gr.Dropdown(models, label='Select your Prediction Model')
+                image_input = gr.Image(type="filepath")
+                # , shape=(224, 224)
+                # text_input = gr.Textbox()    # for testing purposes
+                image_model_input = gr.Dropdown(
+                    models, label='Select your Prediction Model')
                 image_button = gr.Button("Predict")
             with gr.Column():
-                image_output = gr.Textbox()
-                # image_output = gr.Label(num_top_classes=3)
+                text_output = gr.Textbox()
+                image_output = gr.Label(num_top_classes=3)
 
     with gr.Tab("Real-Time Video Streaming"):
         with gr.Row():
             with gr.Column():
                 video_input = gr.Image(source="webcam", streaming=True)
-                video_model_input = gr.Dropdown(models, label='Select your Prediction Model')
+                video_model_input = gr.Dropdown(
+                    models, label='Select your Prediction Model')
                 video_button = gr.Button("Start Prediction")
             with gr.Column():
                 video_output = gr.Label(num_top_classes=3)
-    
 
-    image_button.click(classify, inputs=[image_input, image_model_input], outputs=image_output)
-    video_button.click(test, inputs=[video_input, video_model_input], outputs=[video_output])
+    # image_button.click(test, inputs = text_input, outputs = image_output)    # for testing purposes
+    image_button.click(classify, inputs=[
+                       image_input, image_model_input], outputs=text_output)
+    # video_button.click(test, inputs=[video_input, video_model_input], outputs=[video_output])
 
     gr.Markdown(
         """
